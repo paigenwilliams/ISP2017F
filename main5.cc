10154a715d503734607393dd183ef0da9e65bb24
paigenwilliams <31543226+paigenwilliams@users.noreply.github.com>
2017-12-10 20:41:23 -0600

    Create main5.cc

---
+++ main5.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <valarray>
+#include "Population.h"
+using namespace std;
+
+int main()  {
+
+Population population;
+Person Joe;
+int npeople = 10;
+int step = 0;
+float probability = population.set_probability_of_transfer();
+population.set_population(npeople);
+population.number_immune();
+population.people_met(probability);
+
+
+while (!population.is_pop_stable())  {
+
+population.pop_update();
+
+    float bad_luck = (float) rand()/(float)RAND_MAX;
+    if (bad_luck>.5) {
+      population.random_infection();
+    }
+
+  cout << "In step " << step << ", # sick: " << population.count_infected() << " : ";
+
+  population.print();
+
+  cout << " " << endl;
+
+  step = Joe.set_step(step+1);
+
+  }
+};
