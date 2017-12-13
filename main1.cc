d1ad3e6d595b58109cd94f6411384487dbc9c764
paigenwilliams <31543226+paigenwilliams@users.noreply.github.com>
2017-12-12 19:07:31 -0600

    Fixed formatting issues and unnecessary declarations

--- main1.cc
+++ main1.cc
@@ -9,24 +9,26 @@ int main()  {
 
 Person Joe;
 int step = 0;
-int status_int = 0;
 int n = 0;
 
+    
 while (!Joe.is_stable())  {
 
 Joe.update();
-    float bad_luck = (float) rand()/(float)RAND_MAX;
-    if (bad_luck>.95) {
-      Joe.infect(5);
-    }
 
-  step = Joe.set_step(step+1);
+    
+  float bad_luck = (float) rand()/(float)RAND_MAX;
+  if (bad_luck>.95) {
+    Joe.infect(5);
+  }
+
+  step = Joe.set_step(step++);
 
   if (n == 0)  {
     cout << "On day " << step << ", Joe is " << Joe.status_string() << endl;
   }  else  {
-    cout << "On day " << step << , Joe is " << Joe.status_string() << " ( " << n << " to go)" << endl;
+    cout << "On day " << step << ", Joe is " << Joe.status_string() << " ( " << n << " to go)" << endl;
   }
 
-  cout << "On day " << step << ", Joe is " << Joe.status_string() << endl;
+  }
 };
